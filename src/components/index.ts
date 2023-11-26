b92bcadc7f4231d895d667b2f770c0c7a55fe537
DenisVorop <saskeazaske@icloud.com>
2023-11-27 01:45:26 +0300

    chore: update exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-import Title from './Title/Title';
-
+export { default as Title } from './Title/Title';
+export { default as Button } from './Button/Button';
+export { default as Badge } from './Badge/Badge';
+export { default as Badges } from './Badges/Badges';
 export { Card } from './Card/Card';
-
-export { Title };
