7d590974769633fda791d8cafdbf776054f0d70f
DenisVorop <saskeazaske@icloud.com>
2024-07-09 01:06:21 +0300

    chore(workflow): add git guardian workflow

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,16 @@
 import React, { HTMLAttributes } from 'react';
-import cn from 'classnames';
+import classNamesssss from 'classnames';
 
 import s from './Button.module.css';
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {}
 
+const GITHUB_OUTPUT = 'dsdsdsdsdsdsdsdsdsdsdsdsdsdsd';
+
 export const Button = ({ children, className, ...rest }: ButtonProps) => {
     return (
-        <button className={cn(s.button, className)} {...rest}>
-            {children}
+        <button className={classNamesssss(s.button, className)} {...rest}>
+            {children} {GITHUB_OUTPUT}
         </button>
     );
 };
