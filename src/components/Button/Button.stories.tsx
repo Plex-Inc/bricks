ac959945e66e1c793be6bfd0746c0661ee95aeda
Максим <meefala@MacBook-Air.local>
2024-07-12 17:35:31 +0300

    feat: add `Button` component

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,131 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+    title: 'Example/Button',
+    component: Button,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const svgFilterIcon = (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M13.1111 2H2.88889C2.39797 2 2 2.39797 2 2.88889V2.96514C2 3.20089 2.09365 3.42698 2.26035 3.59368L5.73965 7.07298C5.90635 7.23968 6 7.46578 6 7.70152V11.4506C6 11.7873 6.19022 12.0951 6.49137 12.2457L9.6784 13.8392C9.82615 13.9131 10 13.8056 10 13.6404V7.70152C10 7.46578 10.0937 7.23968 10.2603 7.07298L13.7397 3.59368C13.9064 3.42698 14 3.20089 14 2.96514V2.88889C14 2.39797 13.602 2 13.1111 2Z"
+            stroke="#EBEBED"
+            stroke-linecap="round"
+        />
+    </svg>
+);
+
+export const Ghost: Story = {
+    args: {
+        text: 'Text me',
+        variant: 'primary',
+        view: 'ghost',
+    },
+    argTypes: {
+        variant: {
+            options: ['primary', 'transparent', 'outlined'],
+            control: {
+                type: 'radio',
+            },
+        },
+        size: {
+            options: ['xs', 's', 'm', 'l'],
+            control: {
+                type: 'radio',
+            },
+        },
+    },
+};
+
+export const Warning: Story = {
+    args: {
+        text: 'Text me',
+        variant: 'primary',
+        view: 'warning',
+    },
+    argTypes: {
+        variant: {
+            options: ['primary', 'transparent', 'outlined'],
+            control: {
+                type: 'radio',
+            },
+        },
+        size: {
+            options: ['xs', 's', 'm', 'l'],
+            control: {
+                type: 'radio',
+            },
+        },
+    },
+};
+
+export const Danger: Story = {
+    args: {
+        text: 'Text me',
+        variant: 'primary',
+        view: 'danger',
+    },
+    argTypes: {
+        variant: {
+            options: ['primary', 'transparent', 'outlined'],
+            control: {
+                type: 'radio',
+            },
+        },
+        size: {
+            options: ['xs', 's', 'm', 'l'],
+            control: {
+                type: 'radio',
+            },
+        },
+    },
+};
+
+export const Positive: Story = {
+    args: {
+        text: 'Text me',
+        variant: 'primary',
+        view: 'positive',
+    },
+    argTypes: {
+        variant: {
+            options: ['primary', 'transparent', 'outlined'],
+            control: {
+                type: 'radio',
+            },
+        },
+        size: {
+            options: ['xs', 's', 'm', 'l'],
+            control: {
+                type: 'radio',
+            },
+        },
+    },
+};
+
+const ButtonWithIcon = () => (
+    <div
+        style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '16px',
+        }}
+    >
+        <Button text="Text me" variant="primary" view="ghost" size="l" iconLeft={svgFilterIcon} />
+        <Button text="Text me" variant="primary" view="danger" size="l" iconLeft={svgFilterIcon} />
+        <Button text="Text me" variant="primary" view="positive" size="l" iconLeft={svgFilterIcon} />
+        <Button text="Text me" variant="primary" view="warning" size="l" iconLeft={svgFilterIcon} />
+    </div>
+);
+
+export { ButtonWithIcon };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,115 @@
-import React, { HTMLAttributes } from 'react';
-import classNames from 'classnames';
+import React, { ButtonHTMLAttributes, ReactNode, forwardRef } from 'react';
+import cn from 'classnames';
+
+import { nullable } from '../../utils';
 
 import s from './Button.module.css';
 
-interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {}
-
-export const Button = ({ children, className, ...rest }: ButtonProps) => {
-    return (
-        <button className={classNames(s.button, className)} {...rest}>
-            {children}
-        </button>
-    );
+const variantMap = {
+    primary: s.Button_primary,
+    transparent: s.Button_transparent,
+    outlined: s.Button_outlined,
 };
+
+const variantViewMap = {
+    ghost: {
+        primary: s.Button_ghost_primary,
+        transparent: s.Button_ghost_transparent,
+        outlined: s.Button_ghost_outlined,
+    },
+    warning: {
+        primary: s.Button_warning_primary,
+        transparent: s.Button_warning_transparent,
+        outlined: s.Button_warning_outlined,
+    },
+    danger: {
+        primary: s.Button_danger_primary,
+        transparent: s.Button_danger_transparent,
+        outlined: s.Button_danger_outlined,
+    },
+    positive: {
+        primary: s.Button_positive_primary,
+        transparent: s.Button_positive_transparent,
+        outlined: s.Button_positive_outlined,
+    },
+};
+
+const brickMap = {
+    left: s.Button_brick_left,
+    right: s.Button_brick_right,
+    center: s.Button_brick_center,
+};
+
+const sizeMap = {
+    xs: s.Button_size_xs,
+    s: s.Button_size_s,
+    m: s.Button_size_m,
+    l: s.Button_size_l,
+};
+
+interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'children'> {
+    text?: string;
+    iconLeft?: ReactNode;
+    iconRight?: ReactNode;
+    view?: keyof typeof variantViewMap;
+    variant?: keyof typeof variantMap;
+    brick?: keyof typeof brickMap;
+    size?: keyof typeof sizeMap;
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    (
+        {
+            className,
+            iconLeft,
+            iconRight,
+            text,
+            view = 'ghost',
+            variant = 'primary',
+            brick,
+            size = 's',
+            type = 'button',
+            disabled,
+            ...rest
+        },
+        ref,
+    ) => {
+        const icons = [iconLeft, iconRight].filter(Boolean);
+
+        if (!text && icons.length === 2) {
+            throw new Error("Button can't have two icons without text");
+        }
+
+        const classes = [
+            s.Button,
+            variantMap[variant],
+            variantViewMap[view][variant],
+            sizeMap[size],
+            disabled,
+            brick ? [s.Button_brick, brickMap[brick]] : '',
+            className,
+        ];
+
+        return (
+            <button className={cn(classes)} ref={ref} type={type} {...rest}>
+                {nullable(
+                    !text && icons,
+                    ([icon]) => (
+                        <span className={cn(s.Icon, s.Icon_center)}>{icon}</span>
+                    ),
+                    <>
+                        {nullable(iconLeft, (icon) => (
+                            <span className={cn(s.Icon, s.Icon_left)}>{icon}</span>
+                        ))}
+                        {nullable(text, (t) => (
+                            <span className={s.Text}>{t}</span>
+                        ))}
+                        {nullable(iconRight, (icon) => (
+                            <span className={cn(s.Icon, s.Icon_right)}>{icon}</span>
+                        ))}
+                    </>,
+                )}
+            </button>
+        );
+    },
+);
