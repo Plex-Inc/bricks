f00aa54b443dce5f867fab698e5466f54b67a6ac
DenisVorop <saskeazaske@icloud.com>
2024-07-29 01:39:58 +0300

    ci: update `release.config.js`

--- release.config.js
+++ release.config.js
@@ -1,10 +1,25 @@
 module.exports = {
-    branches: ['main'],
+    branches: [
+        { name: 'main', channel: 'latest' },
+        { name: 'release/**', prerelease: true },
+    ],
+    repositoryUrl: 'https://github.com/Plex-Inc/bricks.git',
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        '@semantic-release/npm',
-        '@semantic-release/git',
+        [
+            '@semantic-release/npm',
+            {
+                npmPublish: true,
+            },
+        ],
+        [
+            '@semantic-release/github',
+            {
+                assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+                successComment: false,
+            },
+        ],
     ],
 };
