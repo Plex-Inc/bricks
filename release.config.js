d114f020563e79691fea72fa424acb1e0c4abc0f
DenisVorop <saskeazaske@icloud.com>
2024-07-29 03:11:52 +0300

    fix: update `release.yml` workflow

--- release.config.js
+++ release.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    branches: [
-        { name: 'main', channel: 'latest' },
-        { name: 'release/**', prerelease: true },
-    ],
+    branches: [{ name: 'main', channel: 'latest' }],
     repositoryUrl: 'https://github.com/Plex-Inc/bricks.git',
     plugins: [
         '@semantic-release/commit-analyzer',
