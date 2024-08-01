eb0628f83232ddbfeb92f2225392ccfce7b98a85
DenisVorop <saskeazaske@icloud.com>
2024-08-02 00:10:42 +0300

    feat: add theme

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { Decorator, Preview } from '@storybook/react';
 import './index.css';
 
 const withTheme: Decorator = (StoryFn, context) => {
     const { theme } = context.globals;
 
-    return (
-        <div
-            style={{
-                position: 'absolute',
-                top: '0',
-                left: '0',
-                right: '0',
-                bottom: '0',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                background: theme === 'dark' ? '#161616' : '#fff',
-            }}
-        >
-            <StoryFn />
-        </div>
-    );
+    useEffect(() => {
+        const docs = document.querySelector('.docs-story');
+
+        if (docs instanceof HTMLElement) {
+            docs.style.backgroundColor = theme === 'dark' ? 'var(--base-background)' : 'var(--text-primary)';
+            docs.style.color = theme === 'dark' ? 'var(--text-primary)' : 'var(--base-background)';
+            docs.style.fontFamily = 'var(--font-family)';
+            return;
+        }
+
+        // if it's not a docs story
+        const html = document.querySelector('html');
+
+        if (html instanceof HTMLElement) {
+            html.style.backgroundColor = theme === 'dark' ? 'var(--base-background)' : 'var(--text-primary)';
+            html.style.color = theme === 'dark' ? 'var(--text-primary)' : 'var(--base-background)';
+            html.style.fontFamily = 'var(--font-family)';
+        }
+    }, [theme]);
+
+    return <StoryFn />;
 };
 
 const preview: Preview = {
