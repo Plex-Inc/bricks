66838d4692e46e140e66b5131ba526294955eb93
Kannolas <vanya.pokorsky@gmail.com>
2024-08-01 12:03:18 +0300

    feat: switch component

---
+++ src/components/Switch/Switch.stories.tsx
@@ -0,0 +1,80 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import React, { useState } from 'react';
+
+import { Switch } from './Switch';
+
+const meta: Meta<typeof Switch> = {
+    title: 'Example/Switch',
+    component: Switch,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const CheckboxDefault: Story = {
+    args: {
+        checked: true,
+        positionText: 'right',
+        label: '',
+    },
+    argTypes: {
+        checked: {
+            options: [true, false],
+            control: {
+                type: 'radio',
+            },
+        },
+        size: {
+            options: ['m', 'l'],
+            control: {
+                type: 'radio',
+            },
+        },
+        positionText: {
+            options: ['right', 'left'],
+            control: {
+                type: 'radio',
+            },
+        },
+    },
+};
+
+const Labeled = () => {
+    const [isChecked, setIsChecked] = useState(true);
+    const [isNotChecked, setIsNotChecked] = useState(false);
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+            <Switch
+                size="l"
+                as="span"
+                label="Switch"
+                checked={isChecked}
+                positionText="right"
+                onChange={(e) => setIsChecked(e.target.checked)}
+            />
+            <Switch
+                size="l"
+                as="span"
+                label="Switch"
+                checked={isNotChecked}
+                positionText="right"
+                onChange={(e) => setIsNotChecked(e.target.checked)}
+            />
+        </div>
+    );
+};
+
+const Disabled = () => {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+            <Switch size="l" disabled as="span" label="Switch" checked={true} positionText="right" />
+            <Switch size="m" disabled as="span" label="Switch" checked={true} positionText="right" />
+        </div>
+    );
+};
+
+export { Labeled, Disabled };
---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,52 @@
+import React, { InputHTMLAttributes } from 'react';
+import cn from 'classnames';
+
+import { Text, type Tags } from '../Text/Text';
+import { nullable } from '../../utils';
+
+import s from './Switch.module.css';
+
+export const sizeMap = {
+    m: s.Switch_size_m,
+    l: s.Switch_size_l,
+};
+
+interface SwitchProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+    checked: boolean;
+    label?: React.ReactNode;
+    positionText?: 'left' | 'right';
+    size: keyof typeof sizeMap;
+    as?: keyof Tags;
+    strong?: boolean;
+}
+
+export const Switch: React.FC<SwitchProps> = ({
+    checked,
+    className,
+    label,
+    as = 'div',
+    size = 'm',
+    positionText = 'right',
+    disabled,
+    strong,
+    ...rest
+}) => {
+    return (
+        <label className={cn(s.SwitchLabel, { [s.SwitchDisabled]: disabled }, className)}>
+            {nullable(label && positionText === 'left', () => (
+                <Text size={size} as={as} strong={strong}>
+                    {label}
+                </Text>
+            ))}
+            <div className={cn(s.SwitchButton, sizeMap[size])}>
+                <input className={cn(s.SliderInput)} type="checkbox" checked={checked} {...rest} />
+                <span className={cn(s.SwitchSlider)}></span>
+            </div>
+            {nullable(label && positionText === 'right', () => (
+                <Text size={size} as={as} strong={strong}>
+                    {label}
+                </Text>
+            ))}
+        </label>
+    );
+};
