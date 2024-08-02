5abdcf17da18ba0acd13d6c1bdd026a26b651cc7
Kannolas <vanya.pokorsky@gmail.com>
2024-08-02 11:48:14 +0300

    feat: avatar component

--- src/utils/getInitials.ts
+++ src/utils/getInitials.ts
@@ -1,5 +1,5 @@
 export const getInitials = (fullName?: string | null) => {
     if (!fullName) return '';
     const arr = fullName.split(' ');
-    return `${arr[0]?.[0] || ''} ${arr[1]?.[0] || ''}`;
+    return `${arr[0]?.[0] || ''} ${arr[1]?.[0] || ''}`.toUpperCase();
 };
