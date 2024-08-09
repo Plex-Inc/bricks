cd812e6dab725ad403c2c589891487f7cbac776c
DenisVorop <saskeazaske@icloud.com>
2024-08-09 23:37:54 +0300

    feat(MenuItem): create component

---
+++ src/components/MenuItem/MenuItem.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { Meta } from '@storybook/react';
+
+import { Text } from '../Text/Text';
+
+import { MenuItem } from './MenuItem';
+
+const meta: Meta<typeof MenuItem> = {
+    title: 'Example/MenuItem',
+    component: MenuItem,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+};
+export default meta;
+
+const MenuItemList = () => {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            {Array.from({ length: 6 }).map((_, index) => (
+                <MenuItem key={index}>
+                    <Text size="s">Item {index + 1}</Text>
+                </MenuItem>
+            ))}
+        </div>
+    );
+};
+
+export { MenuItemList };
