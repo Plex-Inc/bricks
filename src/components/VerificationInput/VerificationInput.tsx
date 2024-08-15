c16761c5ec09443cdd660ea5708477f9a6e74b81
Максим <meefala@MacBook-Air.local>
2024-08-15 18:04:21 +0300

    refactor: verificationInput

--- src/components/VerificationInput/VerificationInput.tsx
+++ src/components/VerificationInput/VerificationInput.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, MutableRefObject, forwardRef } from 'react';
+import React, { InputHTMLAttributes, MutableRefObject, forwardRef, useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 
 import { nullable } from '../../utils';
@@ -27,13 +27,14 @@ interface VerificationInputProps extends Omit<InputHTMLAttributes<HTMLInputEleme
     size?: keyof typeof sizeMap;
 }
 
-interface VerificationInputBox {
-    children: React.ReactNode;
+interface VerificationInputBox extends Omit<VerificationInputProps, 'onChange'> {
     className?: string;
     label?: string;
+    length?: number;
+    onChange: (value: string) => void;
 }
 
-export const VerificationInput = forwardRef<HTMLInputElement, VerificationInputProps>(
+export const Input = forwardRef<HTMLInputElement, VerificationInputProps>(
     ({ className, id, type, view = 'default', size = 's', placeholder, forwardedRef, ...rest }) => {
         return (
             <div ref={forwardedRef}>
@@ -49,17 +50,72 @@ export const VerificationInput = forwardRef<HTMLInputElement, VerificationInputP
     },
 );
 
-export const VerificationBox: React.FC<React.PropsWithChildren<VerificationInputBox>> = ({
-    children,
+export const VerificationInput: React.FC<React.PropsWithChildren<VerificationInputBox>> = ({
     className,
     label,
+    length = 6,
+    onChange,
+    type = 'text',
+    view = 'default',
+    size = 's',
 }) => {
+    const boxRef = useRef<HTMLDivElement | null>(null);
+    const arrayString = Array(length).fill('');
+    const [code, setCode] = useState<string[]>(arrayString);
+
+    const handleChange = (index: number, value: string) => {
+        const newCode = [...code];
+        newCode[index] = value;
+        setCode(newCode);
+
+        if (value.length === 1) {
+            const nextIndex = Math.min(index + 1, length - 1);
+            const inputElement = boxRef.current?.querySelector(`#totp-input-${nextIndex}`);
+            if (inputElement instanceof HTMLInputElement) {
+                inputElement.focus();
+            }
+        }
+        if (!value.length) {
+            const backIndex = Math.min(index - 1, length - 1);
+            const inputElement = boxRef.current?.querySelector(`#totp-input-${backIndex}`);
+            if (inputElement instanceof HTMLInputElement) {
+                inputElement.focus();
+            }
+        }
+
+        onChange(newCode.join(''));
+    };
+
+    useEffect(() => {
+        if (!boxRef.current) return;
+        const inputElement = boxRef.current.querySelector(`#totp-input-${0}`);
+
+        if (inputElement instanceof HTMLInputElement) {
+            inputElement.focus();
+        }
+    }, []);
+
     return (
         <div className={cn(s.Wrapper, className)}>
             {nullable(label, (t) => (
                 <span className={s.Label}>{t}</span>
             ))}
-            <div className={s.InputWrapper}>{children}</div>
+            <div className={s.InputWrapper} ref={boxRef}>
+                {code.map((obj, i) => (
+                    <Input
+                        key={i}
+                        size={size}
+                        type={type}
+                        view={view}
+                        placeholder=""
+                        value={obj}
+                        onChange={(e) => handleChange(i, e.target.value)}
+                        maxLength={1}
+                        inputMode="numeric"
+                        id={`totp-input-${i}`}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
