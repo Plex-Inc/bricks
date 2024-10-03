3b115e7ebf38fc8c0e09c0ab1eb024223166ed90
Максим <meefala@MacBook-Air.local>
2024-10-03 14:12:58 +0300

    refactor: update menuItem

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -3,13 +3,29 @@ import cn from 'classnames';
 
 import s from './MenuItem.module.css';
 
+const sizeMap = {
+    s: s.MenuItem_size_s,
+    m: s.MenuItem_size_m,
+    l: s.MenuItem_size_l,
+};
 interface MenuItemProps extends HTMLAttributes<HTMLDivElement> {
     disabled?: boolean;
+    active?: boolean;
+    size?: keyof typeof sizeMap;
 }
 
-export const MenuItem = ({ children, className, disabled, ...props }: MenuItemProps) => {
+export const MenuItem = ({ children, active, size = 'l', className, disabled, ...props }: MenuItemProps) => {
     return (
-        <div className={cn(s.MenuItem, { [s.MenuItem_disabled]: disabled }, className)} {...props}>
+        <div
+            className={cn(
+                s.MenuItem,
+                sizeMap[size],
+                { [s.MenuItem_disabled]: disabled },
+                { [s.MenuItem_active]: active },
+                className,
+            )}
+            {...props}
+        >
             {children}
         </div>
     );
